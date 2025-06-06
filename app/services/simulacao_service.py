00ff1c50504caf4c241a5dfbca6da78449ea9400
n-honda2 <nicolas.honda@usp.br>
2025-06-06 16:02:00 -0300

    ⚡️ Add service logic

--- app/services/simulacao_service.py
+++ app/services/simulacao_service.py
@@ -13,6 +13,11 @@ def calcular_simulacao(request_data: SimulacaoRequest) -> SimulacaoResponse:
 
     # Implementar dps lógica do cálculo da simulação com os dados do objeot SImulacaoRequest
 
+    valor_entrada = request_data.valor_imovel * (request_data.percentual_entrada / 100)
+    valor_financiado = request_data.valor_imovel - valor_entrada
+    total_a_guardar = request_data.valor_imovel * 0.15
+    parcela_mensal = total_a_guardar / (request_data.anos_contrato * 12)
+
     response = SimulacaoResponse(
         valor_entrada=valor_entrada,
         valor_financiado=valor_financiado,
